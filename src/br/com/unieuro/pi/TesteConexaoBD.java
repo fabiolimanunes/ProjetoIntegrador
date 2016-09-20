2ecc8ea1c7ecd81b218e53c0742182a1388fdcb2
fabio <fabiolimanunes@hotmail.com>
2016-09-20 17:58:07 -0300

    classe de teste de conexão ao BD com uma condição if else e exibindo mensagem joptionpane

--- src/br/com/unieuro/pi/TesteConexaoBD.java
+++ src/br/com/unieuro/pi/TesteConexaoBD.java
@@ -1,22 +1,35 @@
 package br.com.unieuro.pi;
- 
- import java.sql.*;
- import br.com.unieuro.pi.dal.ModuloConexao;
- 
- public class TesteConexaoBD {
- 	// Usando a variável conexão do DAL
- 	static Connection conexao = null;
- 	// Criando variáveis especiais para conexão com o BD
- 	// Prepared Statement e ResultSet são frameworks do pacote java.sql
- 	// que servem para executar as instruções sql
- 	PreparedStatement pst = null;
- 	ResultSet rs = null;
- 
- 	public static void main(String[] args) {
- 		// TODO Auto-generated method stub
- 		conexao = ModuloConexao.conector();
- 		System.out.println(conexao);
- 
- 	}
- 
- }
+
+import java.sql.*;
+
+import javax.swing.JOptionPane;
+
+import br.com.unieuro.pi.dal.ModuloConexao;
+
+public class TesteConexaoBD {
+	// Usando a variável conexão do DAL
+	static Connection conexao = null;
+	// Criando variáveis especiais para conexão com o BD
+	// Prepared Statement e ResultSet são frameworks do pacote java.sql
+	// que servem para executar as instruções sql
+	PreparedStatement pst = null;
+	ResultSet rs = null;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		conexao = ModuloConexao.conector();
+
+		if (conexao == null) {
+			System.out.println("Erro ao conectar ao BD");
+			JOptionPane.showMessageDialog(null, "Erro ao conectar no Banco de Dados!");
+		}
+
+		else {
+			System.out.println("NOME/DRIVER DA CONEXÃO: " + conexao);
+			JOptionPane.showMessageDialog(null, "Sucesso ao conectar no Banco de Dados!");
+
+		}
+
+	}
+
+}
