e06a3d1e45816f327928fd623b1c46da300065e9
fabio <fabio@192.168.2.4>
2016-09-20 15:28:11 -0300

    Testando Conexao BD

---
+++ src/br/com/unieuro/pi/TesteConexaoBD.java
@@ -0,0 +1,22 @@
+package br.com.unieuro.pi;
+
+import java.sql.*;
+import br.com.unieuro.pi.dal.ModuloConexao;
+
+public class TesteConexaoBD {
+	// Usando a variável conexão do DAL
+	Connection conexao = null;
+	// Criando variáveis especiais para conexão com o BD
+	// Prepared Statement e ResultSet são frameworks do pacote java.sql
+	// que servem para executar as instruções sql
+	PreparedStatement pst = null;
+	ResultSet rs = null;
+
+	public void main(String[] args) {
+		// TODO Auto-generated method stub
+		conexao = ModuloConexao.conector();
+		System.out.println("conexao");
+
+	}
+
+}
